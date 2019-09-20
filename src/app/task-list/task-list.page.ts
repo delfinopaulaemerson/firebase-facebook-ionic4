ec4f252fded337d42c8b6f50d099bcfa91871eea
delfinopaulaemerson <delfs@hotmail.com>
2019-09-20 12:43:12 -0300

    mockando lista de tarefas para pre-visualizar a interface

--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,15 +1,21 @@
+import { Task } from 'src/model/task.model';
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.page.html',
-  styleUrls: ['./task-list.page.scss'],
+  styleUrls: ['./task-list.page.scss']
 })
 export class TaskListPage implements OnInit {
+  tasks$: Observable<Task[]>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.tasks$ = of([
+      { id: 'mjhkjhd', title: 'Developer Web', done: false },
+      { id: 'pmpmm', title: 'Developer Hibrido', done: false },
+      { id: 'ascsccas', title: 'Developer Nativo', done: false }
+    ]);
   }
-
 }
