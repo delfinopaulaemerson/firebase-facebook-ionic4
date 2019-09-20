f643f9004a8c7c3d35023d5bddc5d2979d6a3e9c
delfinopaulaemerson <delfs@hotmail.com>
2019-09-20 14:08:53 -0300

    Taskservice firestores

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -1,3 +1,5 @@
+import { User } from './auth.types';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { Injectable } from '@angular/core';
 import { Task } from 'src/model/task.model';
 import { Firestore } from '../firestore.class';
@@ -7,8 +9,18 @@ import { AngularFirestore } from '@angular/fire/firestore';
   providedIn: 'root'
 })
 export class TaskService extends Firestore<Task> {
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, private authService: AuthService) {
     super(db);
-    
+  }
+
+  //cada usuario tem sua propria lista
+  private init(): void {
+    this.authService.authState$.subscribe(user => {
+      if (user) {
+        this.setCollection('/users/' + user.uid + '/tasks');
+        return null;
+      }
+      this.setCollection(null);
+    });
   }
 }
